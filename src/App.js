4c46b8726630d3479b833ed76804777b19b6d78f
jaewon lee <jwleetop5@naver.com>
2023-04-24 00:21:38 +0900

    google-login feat

--- src/App.js
+++ src/App.js
@@ -3,11 +3,27 @@ import { useEffect } from "react";
 import { fireStore } from "./util/firebase";
 
 function App() {
+  function handleFun(res) {
+    console.log("JWT " + res.credential);
+  }
   useEffect(() => {
-    console.log(fireStore);
-  });
+    console.log(fireStore.app.name);
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: process.env.GOOGLE_LOGIN,
+      callback: handleFun,
+    });
 
-  return <div className="App"></div>;
+    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+      theme: "outline",
+      size: "large",
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <div id="signInDiv"></div>
+    </div>
+  );
 }
-
 export default App;
--- src/util/firebase.js
+++ src/util/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,7 +17,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const firebase = initializeApp(firebaseConfig);
-const fireStore = getAnalytics(firebase);
+firebase.initializeApp(firebaseConfig);
+const fireStore = firebase.firestore();
 
 export { fireStore };
